f9d178becfe59eab3c08da8994506c5c6ec15b54
Sohaib <sohaibshaukat78@gmail.com>
2025-03-24 12:46:43 +0500

    Added DrawGrid, DisplayNumber, fixed utils.inc

--- gui/main.asm
+++ gui/main.asm
@@ -4,69 +4,105 @@ option casemap:none
 
 include utils.inc
 
-include \masm32\include\windows.inc
-include \masm32\include\kernel32.inc
-include \masm32\include\user32.inc
-include \masm32\include\gdi32.inc
-
-includelib \masm32\lib\kernel32.lib
-includelib \masm32\lib\user32.lib
-includelib \masm32\lib\gdi32.lib
-
 .data
-    ClassName   db "MainWinClass", 0
-    AppTitle    db "2048 Game", 0
+    ClassName   db "MyWinClass", 0
+    AppTitle    db "2048-The Game", 0
+    hInstance   dd ?
     msg         MSG <>
     wc          WNDCLASS <>
+    hwndMain    dd ?
     hexColor    dd  00ffd0c7h
+    arr dd 16 dup(0)
+    selectedIndex dd 16 dup(?) 
+    num dd 2, 4, 8, 16, 32, 64, 128, 256, 428
+    counter dd 0 
 
 .code
 start:
-    ; Configure Window Class
+    invoke GetModuleHandle, NULL
+    mov hInstance, eax
+
+    ; Setup Window Class
     mov wc.style, CS_HREDRAW or CS_VREDRAW
     mov wc.lpfnWndProc, offset WndProc
-    mov wc.lpszClassName, offset ClassName
-    invoke GetModuleHandle, NULL
+    mov wc.cbClsExtra, 0
+    mov wc.cbWndExtra, 0
+    mov eax, hInstance
     mov wc.hInstance, eax
     invoke LoadIcon, NULL, IDI_APPLICATION
     mov wc.hIcon, eax
     invoke LoadCursor, NULL, IDC_ARROW
     mov wc.hCursor, eax
 
-    ; Background color
-    invoke ConvertHexColor, hexColor
-    invoke CreateSolidBrush, eax
+    ;backgroundColor
+    INVOKE ConvertHexColor, hexColor 
+    INVOKE CreateSolidBrush, eax               
     mov wc.hbrBackground, eax
+    
+    mov wc.lpszMenuName, NULL
+    lea eax, ClassName  ; Use LEA for string addresses
+    mov wc.lpszClassName, eax
 
-    ; Register Class
+    ; Register Window Class
     invoke RegisterClass, addr wc
-    .if eax == 0
-        invoke ExitProcess, 0
-    .endif
+    test eax, eax
+    jz Exit  ; If RegisterClass fails, exit
 
-    ; Create Window
-    invoke CreateWindowEx, 0, addr ClassName, addr AppTitle, WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, 800, 600, NULL, NULL, wc.hInstance, NULL
-    .if eax == NULL
-        invoke ExitProcess, 0
-    .endif
+    invoke CreateWindowEx, 0, addr ClassName, addr AppTitle, \
+                          WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, \
+                          520, 600, NULL, NULL, hInstance, NULL
+    mov hwndMain, eax
+    test eax, eax
+    jz Exit  ; If CreateWindowEx fails, exit
 
-    ; Show & Update Window
-    invoke ShowWindow, eax, SW_SHOWNORMAL
-    invoke UpdateWindow, eax
+    ; Show and update the window
+    invoke ShowWindow, hwndMain, SW_SHOWNORMAL
+    invoke UpdateWindow, hwndMain
 
-    ; Message Loop
-    .while TRUE
-        invoke GetMessage, addr msg, NULL, 0, 0
-        .break .if eax == 0
-        invoke TranslateMessage, addr msg
-        invoke DispatchMessage, addr msg
-    .endw
+MessageLoop:
+    invoke GetMessage, addr msg, NULL, 0, 0
+    cmp eax, 0
+    je Exit
+    invoke TranslateMessage, addr msg
+    invoke DispatchMessage, addr msg
+    jmp MessageLoop
 
+Exit:
     invoke ExitProcess, 0
 
-; Window Procedure
+; Window Procedure - a function that recieves msg from windows like redraw, keyinput etc
 WndProc proc hWnd:DWORD, uMsg:DWORD, wParam:DWORD, lParam:DWORD
-    .if uMsg == WM_DESTROY
+    LOCAL ps:PAINTSTRUCT
+    LOCAL hdc:DWORD
+
+    .if uMsg == WM_PAINT
+        Invoke BeginPaint, hWnd, addr ps
+        mov hdc, eax                   ; Store device context
+
+        INVOKE DrawGrid, hdc
+
+        LOOP1:
+        
+        ; Get second random index (0-8) - selecting number
+        INVOKE GetRandomIndex, 9        ; Returns in edx
+        push edx
+        INVOKE Sleep, 10   
+        mov ecx, OFFSET num             ; Base address of num array
+        pop edx
+        mov eax, [ecx + edx * 4]        ; Get the random number from num array
+        
+        ; Display the number in the selected cell
+        INVOKE DisplayNumber, hdc, counter, eax
+        
+        inc counter
+
+        cmp counter, 16
+        jl LOOP1
+
+        INVOKE EndPaint, hWnd, addr ps
+        xor eax, eax
+        ret
+    .elseif uMsg == WM_DESTROY
         invoke PostQuitMessage, 0
     .else
         invoke DefWindowProc, hWnd, uMsg, wParam, lParam
--- gui/utils.asm
+++ gui/utils.asm
@@ -1,20 +1,26 @@
 .386
-.model flat,stdcall
-.stack 4096
+.model flat, stdcall
+option casemap:none
+
+include utils.inc
 
 .data
-    ; Define any required data variables here
+    fmt db "%d", 0  ; Format for integer conversion
+    hFont dd 0      ; Font handle
+    tickFmt db "Tick Count: %u", 10, 0  ; For Debug
 
 .code
+PUBLIC ConvertHexColor
 
-; Existing ConvertHexColor function
 ConvertHexColor proc color:DWORD
     mov eax, color   ; EAX = 0x00RRGGBB
     mov edx, eax     
     and eax, 0FFh    
     shl eax, 16      
+
     and edx, 0FF00h  
     or eax, edx      
+
     mov ecx, color   
     and ecx, 0FF0000h 
     shr ecx, 16      
@@ -22,57 +28,126 @@ ConvertHexColor proc color:DWORD
     
     ret
 ConvertHexColor endp
-end
 
-CaptureKeyInput proc
-    LOCAL msg:MSG
-KeyLoop:
-    invoke GetMessage, ADDR msg, 0, 0, 0
-    test eax, eax
-    jz ExitKeyLoop
+PUBLIC GetRandomIndex
 
-    cmp msg.message, WM_KEYDOWN
-    jne KeyLoop
+GetRandomIndex proc arrSize:DWORD
+    INVOKE GetTickCount     ;TickCount stored in eax
+    
+    push eax
+    INVOKE printf, ADDR tickFmt, eax            ;This fkr modifies all gen register values, use with caution, save yourself hours of debugging
+    pop eax
 
-    mov eax, msg.wParam
-    .if eax == 'W'
-        jmp MoveUp
-    .elseif eax == 'A'
-        jmp MoveLeft
-    .elseif eax == 'S'
-        jmp MoveDown
-    .elseif eax == 'D'
-        jmp MoveRight
-    .elseif eax == 'Q'
-        invoke ExitProcess, 0
-    .elseif eax == 'R'
-        jmp ResetGame
-    .endif
-
-    jmp KeyLoop
-
-MoveUp:
-    ; Handle moving up
-    jmp KeyLoop
-
-MoveLeft:
-    ; Handle moving left
-    jmp KeyLoop
-
-MoveDown:
-    ; Handle moving down
-    jmp KeyLoop
-
-MoveRight:
-    ; Handle moving right
-    jmp KeyLoop
-
-ResetGame:
-    ; Handle resetting the game
-    jmp KeyLoop
-
-ExitKeyLoop:
+    xor edx, edx
+    mov ebx, arrSize
+    div ebx                 ;Quotient stored in eax, remainder in edx
     ret
-CaptureKeyInput endp
+GetRandomIndex endp
+
+
+
+PUBLIC DrawGrid
+
+DrawGrid proc hdc:DWORD
+    LOCAL i:DWORD
+    LOCAL x:DWORD
+    LOCAL y:DWORD
+    LOCAL cellSize:DWORD
+
+    mov cellSize, 100  ; 
+
+    ; Draw vertical lines (5 lines for 4 cells)
+    mov i, 0
+VLoop:
+    cmp i, 5
+    jge HLoopStart
+
+    mov eax, i
+    mul cellSize
+    add eax, 50  ; Adjusted to start at 50
+    mov x, eax
+
+    invoke MoveToEx, hdc, x, 50, NULL
+    invoke LineTo, hdc, x, 450
+
+    inc i
+    jmp VLoop
+
+HLoopStart:
+    mov i, 0
+HLoop:
+    cmp i, 5
+    jge Done
+
+    mov eax, i
+    mul cellSize
+    add eax, 50  ; Adjusted to start at 50
+    mov y, eax
+
+    invoke MoveToEx, hdc, 50, y, NULL
+    invoke LineTo, hdc, 450, y
+
+    inc i
+    jmp HLoop
+
+Done:
+    ret
+DrawGrid endp
+
+PUBLIC DisplayNumber
+
+DisplayNumber proc hdc:DWORD, cellNo:DWORD, number:DWORD
+    LOCAL strBuffer[12]:BYTE  ; Buffer for number string (max 3 digits + null terminator)
+    LOCAL cellX:DWORD
+    LOCAL cellY:DWORD
+    LOCAL row:DWORD
+    LOCAL col:DWORD
+
+    ; Convert number to string
+    invoke wsprintfA, ADDR strBuffer, ADDR fmt, number
+
+    ; Compute row and column
+    mov eax, cellNo
+    mov edx, 0
+    mov ecx, 4
+    div ecx   ; EAX = row, EDX = col
+
+    mov row, eax
+    mov col, edx
+
+    ; Calculate the center of the cell
+    mov eax, col
+    mov ebx, 100
+    mul ebx
+    add eax, 50  ; Left margin
+    add eax, 35  ; Adjust for better centering
+    mov cellX, eax
+
+    mov eax, row
+    mov ebx, 100
+    mul ebx
+    add eax, 50  ; Top margin
+    add eax, 35  ; Adjust for better centering
+    mov cellY, eax
+
+    ; Set text background to transparent
+    invoke SetBkMode, hdc, TRANSPARENT
+
+    ; Create a larger font if not already created
+    cmp hFont, 0
+    jne FontExists
+    invoke CreateFontA, 40, 0, 0, 0, FW_BOLD, 0, 0, 0, ANSI_CHARSET, OUT_TT_PRECIS, CLIP_DEFAULT_PRECIS, DEFAULT_QUALITY, DEFAULT_PITCH or FF_DONTCARE, NULL
+    mov hFont, eax
+FontExists:
+
+    ; Select the font into DC
+    invoke SelectObject, hdc, hFont
+
+    ; Display the number
+    invoke lstrlenA, ADDR strBuffer
+    invoke TextOutA, hdc, cellX, cellY, ADDR strBuffer, eax
+
+    ret
+DisplayNumber endp
 
 end
